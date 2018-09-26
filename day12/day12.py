6baeee2bba5330f8240b0da81ced6057a0f29224
szaboeman <szab.eman@gmail.com>
2018-09-26 21:11:07 +0200

    2018.09.26

--- day12/day12.py
+++ day12/day12.py
@@ -4,15 +4,23 @@ import json
 def readInput():
     with open("input.txt") as file:
         data = json.load(file)
-    file.close()
-    print(data)
     return data
 
-def solvedA(data):
-   print(list(data.values())[0])
-
+def solvedA(data, red=False):
+    if (isinstance(data,list)):
+        return sum([solvedA(adat,red) for adat in data])
+    elif (isinstance(data, dict)):
+        if (not red or red and not "red" in data.values()):
+            return sum([solvedA(adat,red) for adat in data.values()])
+        else:
+            return 0
+    elif (isinstance(data, str)):
+        return 0
+    elif (isinstance(data, int)):
+        return data
 
 def main():
     data=readInput()
-    solvedA(data)
+    print(solvedA(data, False))
+    print(solvedA(data, True))
 main()
